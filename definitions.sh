46ca71d39db14060b03afba13e5344bb6ba930ae
Tomás Ralph <tomasralph2000@gmail.com>
2021-10-18 05:21:12 -0300

    Updated

--- definitions.sh
+++ definitions.sh
@@ -1,6 +1,62 @@
 #!/bin/bash
 
-function partition_and_mount() {
+function uncomment() {
+
+    # I'm not proud of this but using sed has proven to be a nightmare
+    python - <<EOL
+next_line = $3
+found = False
+with open("$2", 'r') as file:
+    data = file.readlines()
+
+for i, line in enumerate(data):
+    if "$1" == line or found:
+        data[i] = line[1:]
+        if found:
+            break
+        if next_line:
+            found = True
+
+
+with open("$2", 'w') as file:
+    file.writelines(data)
+EOL
+}
+
+function replace() {
+
+    # I'm not proud of this but using sed has proven to be a nightmare
+    python - <<EOL
+with open("$3", 'r') as file:
+    data = file.readlines()
+
+for i, line in enumerate(data):
+    if "$1" == line:
+        data[i] = "$2"
+        break
+
+with open("$3", 'w') as file:
+    file.writelines(data)
+EOL
+}
+
+function configure_pacman() {
+    uncomment '#Color\n' /etc/pacman.conf False
+    uncomment '#VerbosePkgLists\n' /etc/pacman.conf False
+    uncomment '#[multilib]\n' /etc/pacman.conf True
+    replace '#ParallelDownloads = 5\n' 'ParallelDownloads = 10\nILoveCandy\n' /etc/pacman.conf
+}
+
+function configure_locale() {
+    uncomment '#en_US.UTF-8 UTF-8' /etc/locale.gen False
+    uncomment '#es_AR.UTF-8 UTF-8' /etc/locale.gen False
+
+    locale-gen
+
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+}
+
+function partition_and_mount_uefi() {
     timedatectl set-ntp true # sync clock
 
     # disk partitioning
@@ -45,8 +101,39 @@ FDISK_CMDS
 function install_base() {
     pacstrap /mnt base linux linux-firmware
     genfstab -U /mnt >> /mnt/etc/fstab
-    cp -fv locale.gen /mnt/etc/locale.gen
-    cp -fv pacman.conf /mnt/etc/pacman.conf
+}
+
+function partition_and_mount_bios() {
+    timedatectl set-ntp true # sync clock
+
+    # disk partitioning
+    fdisk --wipe always $ROOT_DEVICE << FDISK_CMDS
+n
+
+
+
+w
+FDISK_CMDS
+
+    # partition formatting
+    mkfs.ext4 ${ROOT_DEVICE}1      # root/boot
+
+    # mount partitions
+    mount ${ROOT_DEVICE}1 /mnt
+
+    if [ $STRG_DEVICE ]; then
+        mkdir -pv /mnt/mnt/Storage
+        mount ${STRG_DEVICE} /mnt/mnt/Storage
+    fi
+
+    if [ $WIN_DEVICE ]; then
+        mkdir -pv /mnt/mnt/Windows
+        mount ${WIN_DEVICE} /mnt/mnt/Windows
+    fi
+
+    # get mirrors
+    reflector > /etc/pacman.d/mirrorlist
+    cp -vf pacman.conf /etc/pacman.conf
 }
 
 function setup_network() {
@@ -54,10 +141,10 @@ function setup_network() {
     ln -sfv /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
     hwclock --systohc
 
-    locale-gen
+    configure_locale
 
-    echo "LANG=en_US.UTF-8" > /etc/locale.conf
     echo "${HOSTNAME}" > /etc/hostname
+
     cat >> /etc/hosts <<EOL
 127.0.0.1   localhost
 ::1         localhost
@@ -73,13 +160,19 @@ function prepare_system() {
         os-prober mtools dosfstools git
 
     # install grub
-    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+    if [ "$UEFI" == y ]; then
+        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+    elif [ "$UEFI" == n ]; then
+        grub-install --target=i386-pc $ROOT_DEVICE
+    fi
     grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 function setup_users() {
     useradd -mG wheel,video,audio,optical,storage,games ${USERNAME}
     echo -e ${PASSWD} | passwd ${USERNAME}
+    replace "# %wheel ALL=(ALL) ALL\n" "%wheel ALL=(ALL) ALL\n" /etc/sudoers
+    replace "@includedir /etc/sudoers.d" "@includedir /etc/sudoers.d\n\nDefaults insults" /etc/sudoers
 }
 
 function setup_gui() {
--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 # arch installation script, KDE, GPT partition scheme, xorg
 
-ROOT_DEVICE=/dev/sdX  # replace with desired disk
-PASSWD=
-HOSTNAME=
-USERNAME=
-WIN_DEVICE=/dev/sdX
-STRG_DEVICE=/dev/sdX
+export ROOT_DEVICE=/dev/sdX    # replace with desired disk
+export PASSWD=
+export HOSTNAME=
+export USERNAME=
+export WIN_DEVICE=/dev/sdX     # leave empty if none
+export STRG_DEVICE=/dev/sdX    # leave empty if none
+export UEFI=                   # y/n
 
 source definitions.sh
 
-partition_and_mount
+configure_pacman
+
+if [ $UEFI == y ]; then
+    partition_and_mount_uefi
+elif [ $UEFI == n ]; then
+    partition_and_mount_bios
+else
+    echo "Incorrect value for UEFI variable"
+    exit
+fi
+
 install_base
 
 cp definitions.sh /mnt
@@ -18,6 +29,7 @@ cp definitions.sh /mnt
 # all the following will be ran inside the chroot
 cat << EOF | arch-chroot /mnt
 source definitions.sh
+configure_pacman
 setup_network
 prepare_system
 setup_users
