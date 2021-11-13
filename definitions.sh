6723ae5a1a933c6c389a7a1284e42b58d21554c3
Tomás Ralph <tomasralph2000@gmail.com>
2021-11-13 01:59:38 -0300

    Added interactive setup

--- definitions.sh
+++ definitions.sh
@@ -1,28 +1,102 @@
 #!/bin/bash
 
-function configure_pacman() {
+
+#############
+# PRE-SETUP #
+#############
+setup_variables() {
+
+    echo "\x1b[5;36m"
+    # this is ASCII art
+    base64 -d <<<"H4sIAAAAAAAAA1NQAIF4/Xh9GA1hIdgwGZioggI2eQiNUIsQ5VLAFEdWgdsGhAw2/TA+F8wpEC6y
+YmSH4jMeFaM6nlKnIzyAxem4/IU95NE9iWotuseBxgMAqF41l90BAAA=" | gunzip
+    echo "\x1b[0m"
+    echo
+    echo "Choose the device you want to install Arch Linux on:"
+    echo "\x1b[1;31mThe chosen device will be completely erased and all its data will be lost"
+    echo
+    echo "\x1b[33m"
+    # show the drives in yellow
+    lsblk
+    echo "\x1b[0m"
+    echo
+    echo
+    PS3="Choose the root drive: "
+
+    select drive in $(lsblk | sed '/\(^├\|^└\|^NAME\)/d' | cut -d " " -f 1)
+    do
+        if [ $drive ]; then
+            ROOT_DEVICE="/dev/$drive"
+            break
+        fi
+    done
+
+    PS3="Choose your Windows partition to setup dual-boot: "
+
+    select drive in $(lsblk | sed '/\(^├\|^└\)/!d' | cut -d " " -f 1 | cut -c7-) "None"
+    do
+        if [ "$drive" = "None" ]; then
+            unset WIN_DEVICE
+            break
+        fi
+
+        if [ $drive ]; then
+            WIN_DEVICE="/dev/$drive"
+            break
+        fi
+    done
+
+    PS3="Choose an extra partition to use as Storage: "
+
+    select drive in $(lsblk | sed '/\(^├\|^└\)/!d' | cut -d " " -f 1 | cut -c7-) "None"
+    do
+        if [ "$drive" = "None" ]; then
+            unset STRG_DEVICE
+            break
+        fi
+
+        if [ $drive ]; then
+            STRG_DEVICE="/dev/$drive"
+            break
+        fi
+    done
+
+    read "USERNAME?Enter your username: "
+    read -s "PASSWD?Enter your password: "
+    echo ""
+    read "HOSTNAME?Enter this machine's hostname: "
+}
+
+configure_pacman() {
     sed -i 's/^#Color/Color/' /etc/pacman.conf
     sed -i 's/^#VerboseP/VerboseP/' /etc/pacman.conf
     sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
     sed -i "s/^#ParallelDownloads = 5/ParallelDownloads = 10\nILoveCandy/" /etc/pacman.conf
 }
 
-function configure_locale() {
-    sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-    sed -i 's/^#es_AR.UTF-8 UTF-8/es_AR.UTF-8 UTF-8/' /etc/locale.gen
 
-    locale-gen
+################
+# PARTITIONING #
+################
+partition_and_mount() {
 
-    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+    if [ -d /sys/firmware/efi/efivars ]; then
+        UEFI=y
+        partition_and_mount_uefi
+    else
+        UEFI=n
+        partition_and_mount_bios
+    fi
 }
 
-function partition_and_mount_uefi() {
+partition_and_mount_uefi() {
     timedatectl set-ntp true # sync clock
 
     # disk partitioning
     wipefs --all --force $ROOT_DEVICE
+    # cut removes comments from heredoc
     cut -d " " -f 1 << EOL | fdisk --wipe always --wipe-partitions always $ROOT_DEVICE
-g           # new gpt partition scheme
+g           # gpt partition scheme
 n           # new partition
             # partition number 1
             # start of sector
@@ -59,32 +133,12 @@ EOL
     reflector > /etc/pacman.d/mirrorlist
 }
 
-function configure_nvim() {
-    # init.vim installs plug and plugins automaitcally if it's not there
-    sudo -u ${USERNAME} nvim
-
-    sudo -u ${USERNAME} mkdir -vp ${USR_HOME}/.config/coc/extensions
-    cd ${USR_HOME}/.config/coc/extensions
-    sudo -u ${USERNAME} echo '{"dependencies":{}}'> package.json
-
-    # install extensions
-    sudo -u ${USERNAME} npm install ${COC[@]} --global-style --ignore-scripts\
-        --no-bin-links --no-package-lock --only=prod
-
-    # return to previous directory
-    cd -
-}
-
-function install_base() {
-    pacstrap /mnt ${BASE[@]}
-    genfstab -U /mnt >> /mnt/etc/fstab
-}
-
-function partition_and_mount_bios() {
+partition_and_mount_bios() {
     timedatectl set-ntp true # sync clock
 
     # disk partitioning
     wipefs --all --force $ROOT_DEVICE
+    # cut removes comments from heredoc
     cut -d " " -f 1 << EOL | fdisk --wipe always --wipe-partitions always $ROOT_DEVICE
 n           # new partition
             # primary partition
@@ -115,7 +169,17 @@ EOL
     reflector > /etc/pacman.d/mirrorlist
 }
 
-function setup_network() {
+
+install_base() {
+    pacstrap /mnt ${BASE[@]}
+    genfstab -U /mnt >> /mnt/etc/fstab
+}
+
+
+###########
+# NETWORK #
+###########
+setup_network() {
     # time
     ln -sfv /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
     hwclock --systohc
@@ -132,7 +196,37 @@ EOL
     echo -e "${PASSWD}\n${PASSWD}\n" | passwd
 }
 
-function install_cpu_ucode() {
+configure_locale() {
+    sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+    sed -i 's/^#es_AR.UTF-8 UTF-8/es_AR.UTF-8 UTF-8/' /etc/locale.gen
+
+    locale-gen
+
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+}
+
+
+########
+# BASE #
+########
+prepare_system() {
+    # install basic utilities
+    pacman --noconfirm --needed -Syu ${BASE_APPS[@]}
+
+    install_cpu_ucode
+
+    # install grub
+    if [ "$UEFI" == y ]; then
+        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+    elif [ "$UEFI" == n ]; then
+        grub-install --target=i386-pc $ROOT_DEVICE
+    fi
+
+    configure_grub
+    grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+install_cpu_ucode() {
     CPU=$(lscpu | awk '/Vendor ID:/ {print $3}')
 
     if [ "$CPU" == AuthenticAMD ]; then
@@ -142,7 +236,7 @@ function install_cpu_ucode() {
     fi
 }
 
-function configure_grub() {
+configure_grub() {
 
     # get theme
     git clone \
@@ -166,32 +260,11 @@ function configure_grub() {
     rm -rf /Grub-themes
 }
 
-function prepare_system() {
-    # install basic utilities
-    pacman --noconfirm --needed -Syu ${BASE_APPS[@]}
 
-    install_cpu_ucode
-
-    # install grub
-    if [ "$UEFI" == y ]; then
-        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
-    elif [ "$UEFI" == n ]; then
-        grub-install --target=i386-pc $ROOT_DEVICE
-    fi
-
-    configure_grub
-    grub-mkconfig -o /boot/grub/grub.cfg
-}
-
-function enable_services() {
-    sudo -u ${USERNAME} systemctl enable NetworkManager
-    sudo -u ${USERNAME} systemctl enable sshd
-    sudo -u ${USERNAME} systemctl enable sddm
-    sudo -u ${USERNAME} systemctl enable cups
-    sudo -u ${USERNAME} systemctl enable cronie
-}
-
-function setup_users() {
+#########
+# USERS #
+#########
+setup_users() {
     useradd -mG wheel,video,audio,optical,storage,games -s /bin/zsh ${USERNAME}
     echo -e "${PASSWD}\n${PASSWD}\n" | passwd ${USERNAME}
 
@@ -203,13 +276,25 @@ function setup_users() {
     sed -i "s|@includedir /etc/sudoers.d|@includedir /etc/sudoers.d\n\nDefaults insults|" /etc/sudoers
 }
 
-function detect_drivers(){
+
+#######
+# GUI #
+#######
+setup_gui() {
+    pacman --needed --noconfirm -S ${KDE[@]}
+    detect_drivers
+    pacman --needed --noconfirm -S ${GPU_DRIVERS[@]}
+}
+
+detect_drivers(){
     GPU=$(lspci | grep VGA | cut -d " " -f 5-)
 
     if [[ "${GPU}" == *"NVIDIA"* ]]; then
         GPU_DRIVERS+=('nvidia' 'nvidia-utils' 'lib32-nvidia-utils')
     elif [[ "${GPU}" == *"AMD"* ]]; then
-        GPU_DRIVERS+=('mesa' 'lib32-mesa' 'mesa-vdpau' 'lib32-mesa-vdpau' 'xf86-video-amdgpu' 'vulkan-radeon' 'lib32-vulkan-radeon' 'libva-mesa-driver' 'lib32-libva-mesa-driver')
+        GPU_DRIVERS+=('mesa' 'lib32-mesa' 'mesa-vdpau' 'lib32-mesa-vdpau'\
+            'xf86-video-amdgpu' 'vulkan-radeon' 'lib32-vulkan-radeon'\
+            'libva-mesa-driver' 'lib32-libva-mesa-driver')
     elif [[ "${GPU}" == *"Intel"* ]]; then
         GPU_DRIVERS+=('mesa' 'lib32-mesa' 'vulkan-intel')
     fi
@@ -217,13 +302,29 @@ function detect_drivers(){
     echo "${GPU_DRIVERS[@]}"
 }
 
-function setup_de() {
-    pacman --needed --noconfirm -S ${KDE[@]}
-    detect_drivers
-    pacman --needed --noconfirm -S ${GPU_DRIVERS[@]}
+
+#################
+# CUSTOMIZATION #
+#################
+install_applications() {
+    pacman --needed --noconfirm -S ${APPS[@]}
+    install_paru
+
+    # let the regular user use sudo without password for these commands
+    sed -i "s/^%wheel ALL=(ALL) ALL/# %wheel ALL=(ALL) ALL/" /etc/sudoers
+    sed -i "s/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
+
+    sudo -u ${USERNAME} paru --needed --noconfirm -S ${AUR[@]}
+    install_dotfiles
+    install_powerlevel10k
+    configure_nvim
+
+    # revert the changes
+    sed -i "s/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers
+    sed -i "s/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
 }
 
-function install_paru() {
+install_paru() {
     # use build directory to intall pary as "nobody" user
     # change the directory's group to "nobody" and make it sticky
     # so that all files within get the same properties
@@ -249,27 +350,7 @@ function install_paru() {
     rm -rf /home/build
 }
 
-function install_applications() {
-    pacman --needed --noconfirm -S ${APPS[@]}
-    install_paru
-
-    # let the regular user use sudo without password for these commands
-    sed -i "s/^%wheel ALL=(ALL) ALL/# %wheel ALL=(ALL) ALL/" /etc/sudoers
-    sed -i "s/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
-
-    sudo -u ${USERNAME} paru --needed --noconfirm -S ${AUR[@]}
-    install_dotfiles
-    install_powerlevel10k
-    configure_kde
-    configure_nvim
-    enable_services
-
-    # revert the changes
-    sed -i "s/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers
-    sed -i "s/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
-}
-
-function install_dotfiles() {
+install_dotfiles() {
     git clone https://github.com/tralph3/.dotfiles ${USR_HOME}/.dotfiles
     chmod +x ${USR_HOME}/.dotfiles/install.sh
     chown -R ${USERNAME} ${USR_HOME}
@@ -277,14 +358,36 @@ function install_dotfiles() {
     sudo -u ${USERNAME} ${USR_HOME}/.dotfiles/install.sh
 }
 
-function configure_kde() {
-    sudo -u ${USERNAME} konsave -i ${USR_HOME}/.dotfiles/tralph3.knsv
-    sudo -u ${USERNAME} konsave -a tralph3
-}
-
-function install_powerlevel10k() {
+install_powerlevel10k() {
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${USR_HOME}/.config/powerlevel10k
     chown -R ${USERNAME} ${USR_HOME}
     chgrp -R ${USERNAME} ${USR_HOME}
+}
+
+configure_nvim() {
+    # init.vim installs plug and plugins automatically if it's not there
+    sudo -u ${USERNAME} nvim
+
+    sudo -u ${USERNAME} mkdir -vp ${USR_HOME}/.config/coc/extensions
+    cd ${USR_HOME}/.config/coc/extensions
+    sudo -u ${USERNAME} echo '{"dependencies":{}}'> package.json
+
+    # install extensions
+    sudo -u ${USERNAME} npm install ${COC[@]} --global-style --ignore-scripts\
+        --no-bin-links --no-package-lock --only=prod
+
+    # return to previous directory
+    cd -
+}
+
+
+############
+# SERVICES #
+############
+enable_services() {
+    for service in $SERVICES
+    do
+        systemctl enable $service
+    done
 }
 
--- install.sh
+++ install.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-# arch installation script, KDE, dual-boot (optional)
-
-export ROOT_DEVICE=/dev/sdX    # Drive were Arch will be installed (specify device, not partition)
-export PASSWD=
-export HOSTNAME=
-export USERNAME=
-export WIN_DEVICE=/dev/sdX     # For Windows partition (leave empty if none) (specify partition, not device)
-export STRG_DEVICE=/dev/sdX    # For secondary drive (leave empty if none) (specify partition, not device)
-export UEFI=y                  # y/n
+# tralph3's Arch installation script
 
 source definitions.sh
 source packages.sh
 
+setup_variables
 configure_pacman
-
-if [ $UEFI == y ]; then
-    partition_and_mount_uefi
-elif [ $UEFI == n ]; then
-    partition_and_mount_bios
-else
-    echo "Incorrect value for UEFI variable"
-    exit
-fi
-
+partition_and_mount
 install_base
 
 cat packages.sh definitions.sh > /mnt/definitions.sh
@@ -34,8 +18,9 @@ configure_pacman
 setup_network
 prepare_system
 setup_users
-setup_de
+setup_gui
 install_applications
+enable_services
 exit
 EOF
 
