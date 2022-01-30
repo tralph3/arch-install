1146d3f0c5271f588a0cdf01417315bcb7bdfcda
Tomás Ralph <tomasralph2000@gmail.com>
2022-01-30 01:41:03 -0300

    added descriptions to packages

--- packages.sh
+++ packages.sh
@@ -2,83 +2,83 @@
 
 # core system components
 BASE=(
-    'base'
-    'linux'
-    'linux-firmware'
+    'base'                          # NEEDED: Base Arch Linux system
+    'linux'                         # NEEDED: Linux Kernel
+    'linux-firmware'                # NEEDED: Firmware files for Linux
 )
 
 # basic system components
 BASE_APPS=(
-    'archlinux-keyring'
-    'base-devel'
-    'dialog'
-    'dosfstools'
-    'efibootmgr'
-    'git'
-    'gnu-free-fonts'
-    'grub'
-    'linux-headers'
-    'man-db'
-    'mtools'
-    'network-manager-applet'
-    'networkmanager'
-    'openssh'
-    'os-prober'
-    'python'
-    'reflector'
-    'usbutils'
-    'wget'
-    'xdg-user-dirs'
-    'zsh'
+    'archlinux-keyring'             # NEEDED: Arch Linux PGP key ring
+    'base-devel'                    # OPTIONAL: Various development utilities, needed for Paru and all AUR packages
+    'dialog'                        # NEEDED: Dependency for many TUI programs
+    'dosfstools'                    # OPTIONAL: Utilities for DOS filesystems
+    'efibootmgr'                    # OPTIONAL: Modify UEFI systems from CLI
+    'git'                           # OPTIONAL: Version Control System, needed for the Grub theme, Dotfiles, and Paru
+    'gnu-free-fonts'                # OPTIONAL: Additional system fonts
+    'grub'                          # NEEDED: Bootloader
+    'linux-headers'                 # OPTIONAL: Scripts for building kernel modules
+    'man-db'                        # OPTIONAL: Manual database
+    'mtools'                        # OPTIONAL: Utilities for DOS disks
+    'network-manager-applet'        # OPTIONAL: Applet for managing the network
+    'networkmanager'                # OPTIONAL: Network connection manager
+    'openssh'                       # OPTIONAL: Remotely control other systems
+    'os-prober'                     # OPTIONAL: Scan for other operating systems
+    'python'                        # NEEDED: Essential package for many programs
+    'reflector'                     # OPTIONAL: Get download mirrors
+    'usbutils'                      # OPTIONAL: Various tools for USB devices
+    'wget'                          # OPTIONAL: Utility to download files
+    'xdg-user-dirs'                 # OPTIONAL: Manager for user directories
+    'zsh'                           # OPTIONAL: An alternate shell to bash
 )
 
 # user applications
 APPS=(
-    'alacritty'
-    'android-tools'
-    'audacity'
-    'cronie'
-    'cups'
-    'exa'
-    'ffmpeg'
-    'firefox'
-    'flameshot'
-    'gimp'
-    'htop'
-    'mlocate'
-    'mpv'
-    'neofetch'
-    'neovim'
-    'neovim-plug'
-    'nerd-fonts-ubuntu-mono'
-    'numlockx'
-    'p7zip'
-    'qbittorrent'
-    'unrar'
-    'unzip'
-    'xclip'
-    'zenity'
-    'zip'
-    'zsh-autosuggestions'
-    'zsh-syntax-highlighting'
-    'zsh-theme-powerlevel10k'
+    'alacritty'                     # OPTIONAL: Hardware accelerated terminal emulator
+    'android-tools'                 # OPTIONAL: Utilities for managing android devices
+    'audacity'                      # OPTIONAL: Audio editor
+    'cronie'                        # OPTIONAL: Task scheduler
+    'cups'                          # OPTIONAL: Printer manager
+    'exa'                           # OPTIONAL: Replacement for the ls command
+    'ffmpeg'                        # OPTIONAL: Audio and video magic
+    'firefox'                       # OPTIONAL: Web browser
+    'flameshot'                     # OPTIONAL: Screenshot utility
+    'gimp'                          # OPTIONAL: Image editor
+    'htop'                          # OPTIONAL: System and process manager
+    'mlocate'                       # OPTIONAL: Quickly find files and directories
+    'mpv'                           # OPTIONAL: Suckless video player
+    'neofetch'                      # OPTIONAL: Display system information, with style
+    'neovim'                        # OPTIONAL: Objectively better than Emacs
+    'neovim-plug'                   # OPTIONAL: Plugin manager for neovim
+    'nerd-fonts-ubuntu-mono'        # OPTIONAL: Nice fonts for the terminal
+    'numlockx'                      # OPTIONAL: Set numlock from CLI
+    'p7zip'                         # OPTIONAL: Support for 7zip files
+    'qbittorrent'                   # OPTIONAL: Torrent downloader
+    'unrar'                         # OPTIONAL: Support for rar files
+    'unzip'                         # OPTIONAL: Support for zip files
+    'xclip'                         # OPTIONAL: Copy to clipboard from CLI
+    'zenity'                        # OPTIONAL: Basic GUIs from CLI
+    'zip'                           # OPTIONAL: Support for zip files
+    'zsh-autosuggestions'           # OPTIONAL: Suggest commands as you type for zsh
+    'zsh-syntax-highlighting'       # OPTIONAL: Syntax highlighting for zsh
+    'zsh-theme-powerlevel10k'       # OPTIONAL: Stylish prompt for zsh
 )
 
 GAMING_APPS=(
-    'discord'
-    'gamemode'
-    'lib32-gamemode'
-    'lutris'
-    'mangohud'
-    'proton-ge-custom-bin'
-    'steam'
-    'steam-fonts'
-    'steam-metadata-editor-git'
-    'steam-native-runtime'
-    'wine'
-    'wine-gecko'
-    'wine-mono'
-    'winetricks'
+    'discord'                       # OPTIONAL: Communication software
+    'gamemode'                      # OPTIONAL: System optimizations for gaming
+    'lib32-gamemode'                # OPTIONAL: Same, but 32bit
+    'lutris'                        # OPTIONAL: Game launcher and configuration tool
+    'mangohud'                      # OPTIONAL: HUD for monitoring system and logging
+    'proton-ge-custom-bin'          # OPTIONAL: Extra fixes and patches for Proton
+    'steam'                         # OPTIONAL: Game storefront
+    'steam-fonts'                   # OPTIONAL: Fonts for that weird game storefront
+    'steam-metadata-editor-git'     # OPTIONAL: Edit metadata for your Steam games
+    'steam-native-runtime'          # OPTIONAL: A native runtime for the weird storefront
+    'wine'                          # OPTIONAL: Run Windows applications on Linux
+    'wine-gecko'                    # OPTIONAL: Wine's replacement for Internet Explorer
+    'wine-mono'                     # OPTIONAL: Wine's replacement for .Net Framework
+    'winetricks'                    # OPTIONAL: Script to install libraries in Wine
 )
 
 # all of these will get enabled
